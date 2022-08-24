267c9df478af83bc8c498e71b6dfcbbe526c5199
Nataly Sintigo <nataly_sintigo@hotmail.com>
2022-08-23 23:59:30 -0600

    delete doble db connection

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 require('dotenv').config()
+const {getComments, createComment, updateComment, deleteComment} = require('./controllers').Comment
+require('./controllers').Comment
 require('./db').connect()
-
-/*const routes = require("./routes/comments")*/
-
-
-const {getComments, createComment, updateComment, deleteComment} = require('./controllers')
-
 const express = require('express')
-const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
 const app = express()
-
-const { DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env
-
-const uri = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.hlhgj.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
-
-mongoose.connect(uri, {
-    // @ts-ignore
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).catch(error=> console.log(error))
-
 
 const PORT = process.env.PORT || 3300
 
 /* Middleware */
 app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+app.use(cookieParser())
 
-
+/* 404 route */
+app.get('*', (req, res) => res.send('404 Not Found'))
 app.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}`)
 })
